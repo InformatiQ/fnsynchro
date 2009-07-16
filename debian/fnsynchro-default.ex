a8011ad9dc49a256be9c001170c276a11e98102f
Ramez Hanna <rhanna@paanch.research.nokia.com>
2009-07-16 14:11:57 +0300

    added debian dir

---
+++ debian/fnsynchro-default.ex
@@ -0,0 +1,10 @@
+# Defaults for fnsynchro initscript
+# sourced by /etc/init.d/fnsynchro
+# installed at /etc/default/fnsynchro by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(fnsynchro):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
+  title="fnsynchro" command="/usr/bin/fnsynchro"
